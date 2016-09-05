6f81fe0b55d3b09d3235077b5bdc8e9fc701f360
Bernardo Pollak <bpollakw@gmail.com>
2016-09-05 23:24:17 +0100

    Start of DNArchive

--- app.py
+++ app.py
@@ -11,92 +11,20 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation, CompoundLocation
 from Bio.Alphabet import IUPAC
 
 app = Flask(__name__, static_url_path="", static_folder="static")
-app.secret_key = 'HJKDGSA&^D%HJKN.zczxcoasdk2194uru'
 
 
-#@app.errorhandler(404)
-#def page_not_found(e):
-#    return render_template('404.html', title='404'), 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', title='404'), 404
 
 @app.errorhandler(500)
 def internal_error(e):
 	return render_template('500.html', title='500'), 500
 
-# Helper function for recursive search of restriction sites
-			
-def recfind(pattern, string, where_should_I_start=0):
-    # Save the result in a variable to avoid doing the same thing twice
-    pos = string.find(pattern, where_should_I_start)
-    if pos == -1:
-        # Not found!
-        return []
-    # No need for an else statement
-    return [pos] + recfind(pattern, string, pos + len(pattern))
-
-
 @app.route('/')
 def index():
 				
 	return render_template('home.html')
-
-@app.route('/recode', methods=['POST'])
-def recode():
-	
-	if request.method == 'POST':
-		seq = request.form["seq"]
-		seq = seq.upper()
-		seq = re.sub(' ','',seq).strip()
-		seqtype = request.form["seqtype"]
-		name = request.form["name"]
-	
-	BsaI_F = "GGTCTC"
-	BsaI_F_replace = ["GGaCTC","GGTCcC","GGTaTC"]
-	BsaI_R = "GAGACC"
-	BsaI_R_replace = ["GAaACC","GAGAtC" ,"GAGgCC"]
-	SapI_F = "GCTCTTC"
-	SapI_F_replace = ["GCcCTTC","GCTCgTC","GCTgTTC"]
-	SapI_R = "GAAGAGC"
-	SapI_R_replace = ["GAgGAGC","GAAGgGC","GAAaAGC"]
-	
-#	ATGGGTCTCAGGTCTCAAGGTCTCAAAGGTCTCGGTCTCGGTCTCGAGACCAAGAGACCAAAGAGACCGAGACCAGAGACCGAGACCGCTCTTCAGCTCTTCAAGCTCTTCAAAGCTCTTCGAAGAGCAGAAGAGCAAGAAGAGCAAAGAAGAGCGAAGAGC
-	
-	BsaI_sitesF = []
-	BsaI_sitesR = []
-	SapI_sitesF = []
-	SapI_sitesR = []
-	
-	BsaI_sitesF = BsaI_sitesF + recfind(BsaI_F, seq)
-	BsaI_sitesR = BsaI_sitesR + recfind(BsaI_R, seq)
-	SapI_sitesF = SapI_sitesF + recfind(SapI_F, seq)
-	SapI_sitesR = SapI_sitesR + recfind(SapI_R, seq)
-#	for search in SapI:
-#		SapI_sites = SapI_sites + recfind(search, seq)
-	orgseq = seq
-	for site in BsaI_sitesF:
-		for i in range(3):
-			if (site % 3 == i):
-				seq = seq[:site] + BsaI_F_replace[i] + seq[site + 6:]
-	
-	for site in BsaI_sitesR:
-		for i in range(3):
-			if (site % 3 == i):
-				seq = seq[:site] + BsaI_R_replace[i] + seq[site + 6:]
-	
-	for site in SapI_sitesF:
-		for i in range(3):
-			if (site % 3 == i):
-				seq = seq[:site] + SapI_F_replace[i] + seq[site + 7:]
-				
-	for site in SapI_sitesR:
-		for i in range(3):
-			if (site % 3 == i):
-				seq = seq[:site] + SapI_R_replace[i] + seq[site + 7:]
-					
-	BsaI_sites = BsaI_sitesF+BsaI_sitesR
-	SapI_sites = SapI_sitesF+SapI_sitesR
-	
-
-	return render_template('recode.html', seq=orgseq, newseq=seq, seqtype=seqtype, name=name,BsaI=BsaI_sites, SapI=SapI_sites)
 
 @app.route('/export')
 def export():
